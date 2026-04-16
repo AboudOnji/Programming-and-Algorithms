f906d2f6072b285c69c2375861ccfdf6cc1afc27
AboudOnji <aboudonji@gmail.com>
2026-04-16 13:29:47 -0600

    new graphics example

--- Graphics.py
+++ Graphics.py
@@ -1,9 +1,25 @@
 #código para diseño gráfico
 
 import tkinter as tk
+
+def saludar():
+    nombre = entrada_nombre.get()
+    etiqueta_resultados.config(text="Hola " + nombre)
+
+
 ventana = tk.Tk()
-etiqueta = tk.Label (ventana, text="Hola mundo")
-etiqueta.pack()
+ventana.title("Mi aplicación")
+ventana.geometry("400x300")
+ventana.config(bg="blue")
+etiquetainst=tk.Label(ventana, text="Escribe tu nombre")
+entrada_nombre = tk.Entry(ventana)
+boton_saludar = tk.Button(ventana, text="Saludar", command=saludar)
+etiqueta_resultados=tk.Label(ventana, text="")
+
+etiquetainst.pack(pady=10)
+entrada_nombre.pack(pady=10)
+boton_saludar.pack(pady=10)
+etiqueta_resultados.pack(pady=10)
 
 ventana.mainloop()
 
